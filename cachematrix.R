a3cc6a716ec6c408e23669a18643a808f2f5f70e
Kevin Christopher Unger <30776398+KungerPDX@users.noreply.github.com>
2017-09-17 11:38:43 -0700

    Additional commentary added

--- cachematrix.R
+++ cachematrix.R
@@ -5,14 +5,26 @@
 ##  closures. Does not compute the inverse automatically; it relies on the caller for that.
 
 makeCacheMatrix <- function(x = matrix()) {
+        
+        # Lexically scoped inverse variable
         inv <- NULL
+        
+        # Set function for the matrix
         set <- function(y) {
                 x <<- y
                 inv <<- NULL
         }
+        
+        # Get function for the matrix
         get <- function() x
+        
+        # Set function for the inverse
         setinverse <- function(inverse) inv <<- inverse
+        
+        # Get function for the inverse
         getinverse <- function() inv
+        
+        # List function
         list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
@@ -28,13 +40,17 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         
+        # Inverse for the cached matrix
         inv <- x$getinverse()
         
+        # If inverse is set, return it along with a message that it was in the cache
         if(!is.null(inv)) {
                 message("getting cached data")
                 return(inv)
         }
         
+        # Otherwise, return the data, calculate the inverse, set it in the cache, and
+        # inverse variable from function entry
         data <- x$get()
         inverse <- solve(data, ...)
         x$setinverse(inverse)
